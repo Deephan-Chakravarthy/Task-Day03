93a780692ab0f7584dd63aac7bb7f45b75c74fae
Deephan-Chakravarthy <deephan221099@gmail.com>
2024-01-26 16:23:56 +0530

    qno1

--- Qno1.js
+++ Qno1.js
@@ -2,7 +2,7 @@
 //Ans: It can be compared using isEqual() method 
 
 
-var obj1 = { "name":"Person 1", "age":5};
-var obj2 = { "age":5, "name":"Person 1"};
+var obj1 = { name:"Person 1", age:5};
+var obj2 = { age:5, name:"Person 1"};
 
 console.log(isEqual(obj1,obj2));
